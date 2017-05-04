1c48cf536703e39813eb2eec3e645cd19de03d9b
maribel <maribel.diaz.j@gmail.com>
2017-05-04 14:40:31 -0300

    Ejercicio practico 1 completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,10 +24,40 @@ function Pokemon (nombre,color,poderDeAtaque){
 
    }
    
-const Pikachu = new Pokemon ("Pikachu", "amarillo",100);
+   const Pikachu = new Pokemon ("Pikachu", "amarillo",100);
    const Charmander = new Pokemon ("Charmander", "rojo",20);
    const Zaidoc = new Pokemon ("Zaidoc", "amarillo",50);
+   const Bulbasour = new Bulbasour("Bulbasour", "verde", 70);
      
      Pikachu.atacar (Charmander);
      console.log(Charmander.vida);
+
+ function botonPelear(){
+
+   var pokemon1 = document.getElementById("Pok1").value;
+   var pokemon2 = document.getElementById("Pok2").value;
+
+   var registro = document.getElementById("res");
+   
+   var nuevoPok = new Pokemon (pokemon1,"azul",30);
+   var nuevoPok2= new Pokemon (pokemon2,"rojo",60);
+
+   if(pokemon1 == pokemon2){
+      alert("Pokemones iguales no pueden luchar");
+
+   }else{
+      nuevoPok.atacar(nuevoPok2);
+      var resultado = (pokemon1 + " ataco a " + pokemon2 + " y " + pokemon2 + " tiene una vida de: "+ nuevoPok2.vida + ".");
+      registro.innerHTML = resultado
+   }
+
+
+ }
+
+
+
+
+
+
+
 
